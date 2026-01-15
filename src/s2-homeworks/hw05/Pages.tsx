90db3c912e178b61718c06823965e2538b8fa794
Eduards <eduards.fogelis1990@gmail.com>
2026-01-15 20:19:20 +0200

    HOME WORK

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -16,11 +16,11 @@ function Pages() {
         <div>
             {/*Routes выбирает первый подходящий роут*/}
             <Routes>
-                {/*роутинг будут писать студенты*/}
-                {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
+            {/*    /!*роутинг будут писать студенты*!/*/}
+            {/*    /!*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*!/*/}
                 <Route path="/" element={<Navigate to = {PATH.PRE_JUNIOR}/> } />
 
-                {/*роуты для /pre-junior, /junior, /junior-plus*/}
+            {/*    /!*роуты для /pre-junior, /junior, /junior-plus*!/*/}
                 <Route path="/pre-junior"  element={<PreJunior />} />
                 <Route path="/junior" element={<Junior />} />
                 <Route path="/junior-plus" element={<JuniorPlus />} />
